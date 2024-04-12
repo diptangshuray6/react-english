accc9e2da15fa328e69f5bc677f93fca2b10f17c
diptangshuray6 <115389855+diptangshuray6@users.noreply.github.com>
2024-04-12 22:18:29 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import products from './products';
+
+
+function App() {
+  return (
+    <>
+      <h3>hello from react</h3>
+      <products />
+   </>
+  );
+}
+export default App;
---
+++ products.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function products() {
+  return (
+   <div color='white'>product vs</div>
+  )
+}
+
+export default products;
